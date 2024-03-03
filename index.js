3e9a06d96806404e2f4d008b1f7f88f677c31a78
David Chan <mrtranduc1994@outlook.com>
2024-03-03 15:14:30 +0700

    demo

---
+++ index.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#example1").inputmask("99-9999999");
+    $("#example2").inputmask();
+
+
+    $("#example3").inputmask();
+    $("#example4").inputmask();
+  });
